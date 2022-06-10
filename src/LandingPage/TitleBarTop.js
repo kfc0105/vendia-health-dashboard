c9cc516008e93527c43004bca471a226abd1bfdb
danielSmagly <77174573+danielSmagly@users.noreply.github.com>
2022-06-10 16:54:46 -0700

    Update TitleBarTop.js
    Improved visuals.

--- src/LandingPage/TitleBarTop.js
+++ src/LandingPage/TitleBarTop.js
@@ -5,15 +5,16 @@ import styled from 'styled-components';
 function TitleBarTop() {
     return (
         <STYLE_1>
-            <Typography variant='h4'> Main Page </Typography>
+           <h1>Welcome to Vendia Health Dashboard</h1>
         </STYLE_1> 
     );
 };
 
 const STYLE_1 = styled.div`
 
-    background-color: #42fcd7;
+    background-image: linear-gradient(144deg,#adffd0, #8afff7 50%,#a3d9ff);
     text-align: center;
+    font-family: 'Ubuntu', sans-serif;
 
 `
 
