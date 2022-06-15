561f410d31c6c9b5938967019da4f4dc9f14df02
Justinroome <63683267+Justinroome@users.noreply.github.com>
2022-06-14 17:29:48 -0700

    Added StatisticsReportModal
    This popup modal will be triggered by the "Statistics Report" button in the ViewData component. It will display a data summary of the selected DataGrid rows. The data summary will consist of all the data analysis listed in the project requirements. It is NOT integrated with ViewData at this point

---
+++ src/ViewData/StatisticsReportModal.js
@@ -0,0 +1,47 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
+export default function AlertDialog() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Statistics Report
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="statistics-report-title"
+        aria-describedby="statistics-report"
+      >
+        <DialogTitle id="statistics-report-title">
+          {"Employee Data Report"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="report">
+            Populate data report here
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} autoFocus>
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
