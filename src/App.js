89ed2cc3aa90b1d24e95cb57cf2c7acc5104a92c
Bryan Burch <bburch@csus.edu>
2022-06-17 15:35:21 -0700

    Setup vendia client

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import LandingPage from './LandingPage/LandingPage.js'
 
+import { createVendiaClient } from '@vendia/client';
+
+const myApiKey = `2edQCbZNsmLrogFXEsPPdwcUeGxhJzicaeyjbThNCYMV`;
+
+const client = createVendiaClient({
+  apiUrl: `https://aqzfdiwh32.execute-api.us-west-2.amazonaws.com/graphql/`,
+  websocketUrl: `wss://96eoo7yd7g.execute-api.us-west-2.amazonaws.com/graphql`,
+  apiKey: myApiKey,
+});
+
 export default function App() {
   return (
     <LandingPage/>
   );
 };
+
+export { client };
